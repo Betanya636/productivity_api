b90788c3fedd40dce52d027d2b83917f3be0afac
Betanya636 <dnr3mk@virginia.edu>
2025-11-24 12:58:32 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="src/templates")
 CORS(app)
 
 # In-memory list of tasks with seed/test data
